7c8b823f47a907e827b84c42837052d4d5b5f2b9
Marcos001 <santosMsantos01@gmail.com>
2017-04-02 17:35:38 -0300

    Version 1 update

---
+++ app/src/main/java/com/trairas/nig/peer_to/Util/util.java
@@ -0,0 +1,17 @@
+package com.trairas.nig.peer_to.Util;
+
+import android.util.Log;
+
+/**
+ * Created by nig on 02/04/17.
+ */
+
+public class util {
+
+    public void util(){}
+
+    public void print(String message){
+        Log.v(".--------> ",message);
+    }
+
+}
