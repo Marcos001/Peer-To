b3596074c951a16699dc5a35c047c43b87586ed0
Marcos001 <santosMsantos01@gmail.com>
2017-04-02 18:40:26 -0300

    update

--- app/src/main/java/com/trairas/nig/peer_to/_main_.java
+++ app/src/main/java/com/trairas/nig/peer_to/_main_.java
@@ -3,6 +3,7 @@ package com.trairas.nig.peer_to;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.trairas.nig.peer_to.Fragmentos.CriarPalavra;
 import com.trairas.nig.peer_to.Util.*;
 
 public class _main_ extends AppCompatActivity
@@ -91,14 +93,17 @@ public class _main_ extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        FragmentManager frag = getSupportFragmentManager();
+
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            ut.print("Criar palavra");
+            frag.beginTransaction().replace(R.id.conteudo, new CriarPalavra()).commit();
         } else if (id == R.id.nav_gallery) {
-
+            ut.print("Pesquisar palavra");
         } else if (id == R.id.nav_slideshow) {
-
+            ut.print("Add Peer");
         } else if (id == R.id.nav_manage) {
-
+            ut.print("?");
         }/* else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
