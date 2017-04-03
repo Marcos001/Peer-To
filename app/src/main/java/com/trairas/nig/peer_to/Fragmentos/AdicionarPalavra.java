d2becb4347fd98a1f8c03c7c3051c5e77b69ff8d
Marcos001 <santosMsantos01@gmail.com>
2017-04-03 00:17:25 -0300

    update

--- app/src/main/java/com/trairas/nig/peer_to/Fragmentos/AdicionarPalavra.java
+++ app/src/main/java/com/trairas/nig/peer_to/Fragmentos/AdicionarPalavra.java
@@ -7,22 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.trairas.nig.peer_to.R;
 
 
 public class AdicionarPalavra extends Fragment {
 
+    TextView tv;
+    Button bt;
+    EditText ed;
+
+
     public AdicionarPalavra() {
         // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_adicionar_palavra, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_adicionar_palavra, container, false);
+
+        tv = (TextView) view.findViewById(R.id.add_p_tv_1);
+        tv.setText(R.string.cp_tv_title);
+        ed = (EditText) view.findViewById(R.id.add_p_edt_1);
+        ed.setText(R.string.hello_blank_fragment);
+        bt = (Button) view.findViewById(R.id.add_p_bt_1);
+        bt.setText(R.string.cp_bt_add);
+
+        return (view);
     }
 
 }
